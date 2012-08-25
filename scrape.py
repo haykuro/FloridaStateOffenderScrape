e382918e44f50ab033d4021ae2b71dccaf3c11f7
Steve Birstok <steve@stevebirstok.com>
2012-08-25 03:38:50 -0400

    Updated Caching + Offender Iteration Start
    This commit moves caching from the Main execution into the GetURL
    function (so it's now smart enough to cache on its own based on URLs).
    I used a similar scheme to what CodeIgniter does for this
    functionality. (md5 the urls).
    
    This commit also introduces new code which iterates and pulls content
    for inmate detail pages.

--- scrape.py
+++ scrape.py
@@ -1,10 +1,20 @@
-# imports
+### IMPORTS ###
+# regular expressions
+import re
+# md5 sums
+import md5
+# Network IO
 import urllib2
+# URL Parser
 from urlparse import urlparse
+# BeautifulSoup for Scraping
 from bs4 import BeautifulSoup
+### END IMPORTS ###
 
+### VARIABLES ###
 # URLs
 inmatePopulationSearchURL = 'http://www.dc.state.fl.us/ActiveInmates/list.asp?DataAction=Filter&SexOffOnly=0&dcnumber=&LastName=&FirstName=&SearchAliases=on&Sex=M&Race=ALL&OffenseCategory=ALL&ClassificationStatus=ALL&CurrentCustody=ALL&IdentifierType=ALL&Identifier=&EyeColor=ALL&HairColor=ALL&FromAge=&ToAge=&FromWeight=&ToWeight=&FromHeightFeet=&FromHeightInches=&ToHeight=&ToHeightFeet=&ToHeightInches=&ZipCode=&ScarType=ALL&ScarLocation=ALL&ScarDescription=&photosonly=on&nophotos=on&items=50&CommitmentCounty=ALL&subjecttype=ALL&facility=ALL&workskill=ALL'
+### END VARIABLES ###
 
 
 # classes, functions
@@ -18,10 +28,34 @@ class OffenderScraper:
         else:
             url = urlschema.geturl()
 
-        response = urllib2.urlopen(url)
-        return response.read()
+        content = ""
+        # Caching based on md5's of URL's
+        cacheFileName = md5.new(url).hexdigest()
+        try:
+            # try to open cache
+            cache = open('/tmp/' + cacheFileName, 'r+b')
+            cacheContents = cache.read()
+            cache.close()
 
-    def ScrapeTable(self, htmlContent):
+            if(cacheContents.__len__ > 0):
+                # cache is not blank
+                content = cacheContents
+            else:
+                # cache is blank, fill it.
+                response = urllib2.urlopen(url)
+                content = response.read()
+                cache.write(content)
+        except IOError:
+            # cache does not exist, create it.
+            cache = open('/tmp/' + cacheFileName, 'w+b')
+            response = urllib2.urlopen(url)
+            content = response.read()
+            cache.write(content)
+            cache.close()
+
+        return content
+
+    def ScrapeTableLinks(self, htmlContent):
         soup = BeautifulSoup(htmlContent, 'lxml')
         contentContainer = soup.find(id="dcCSScontentContainer")
         table = contentContainer.find_all('table')[2]
@@ -30,10 +64,13 @@ class OffenderScraper:
         for item in ths:
             headers.append(item.get_text())
 
-        links = table.find_all('a')
-        for link in links:
-            print link.get('href')
-        return headers
+        HTMLlinks = table.find_all('a')
+        links = []
+        for link in HTMLlinks:
+            # Only parse links that lead to an offender detail page.
+            if "detail.asp" in link.get('href'):
+                links.append(link.get('href'))
+        return links
 
 
 # Main App Function
@@ -41,23 +78,14 @@ class Main():
     def __init__(self):
         DataScraper = OffenderScraper()
 
-        # Pull Data
-        try:
-            cache = open('/tmp/populationSearch', 'r+b')
-            cacheContents = cache.read()
-            cache.close()
+        # ScrapeTableLinks
+        DetailPageLinks = DataScraper.ScrapeTableLinks(DataScraper.GetURL(inmatePopulationSearchURL))
 
-            if(cacheContents.__len__ > 0):
-                    content = cacheContents
-            else:
-                    content = DataScraper.GetURL(inmatePopulationSearchURL)
-        except IOError:
-            cache = open('/tmp/populationSearch', 'w+b')
-            content = DataScraper.GetURL(inmatePopulationSearchURL)
-            cache.write(content)
-            cache.close()
-
-        print DataScraper.ScrapeTable(content)
+        # Get Offender Data
+        for link in DetailPageLinks:
+            content = DataScraper.GetURL('http://www.dc.state.fl.us/ActiveInmates/' + link)
+            print content
+            break
         return None
 
 

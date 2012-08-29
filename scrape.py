87dc4dbad82b11682714a8471be224a24ea6282e
Steve Birstok <steve@stevebirstok.com>
2012-08-28 22:01:34 -0400

    Fixed Caching + Iterate for All Results
    Caching had a bug where it attempted to write to a closed file handler.
    
    Updates made so the scraper now iterates through ALL results in the
    given session.

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@
 
 ### IMPORTS ###
 # regular expressions
-# import re
+import re
 # os path functions
 import os
 # md5 sums
@@ -48,7 +48,6 @@ class OffenderScraper:
             # try to open cache
             cache = open(cacheDir + cacheFileName, 'r+b')
             cacheContents = cache.read()
-            cache.close()
 
             if(len(cacheContents) > 0):
                 # cache is not blank
@@ -58,6 +57,7 @@ class OffenderScraper:
                 response = urllib2.urlopen(url)
                 content = response.read()
                 cache.write(content)
+                cache.close()
         except IOError:
             # cache does not exist, create it.
             cache = open(cacheDir + cacheFileName, 'w+b')
@@ -87,59 +87,94 @@ class OffenderScraper:
                     links.append(link.get('href'))
         return links
 
+    def GetInmateDetails(self, detailContent, onlyShowRecords=False):
+        """Scrapes Offender Detail Page"""
+        soup = BeautifulSoup(detailContent, 'lxml')
+        contentContainer = soup.find(id="dcCSScontentContainer")
+
+        if onlyShowRecords == True:
+            # Use regular expressions to see how many records this page reports there are
+            matches = re.search("Record: ([\d\-,]+) of ([\d\-,]+)", str(contentContainer))
+            return matches.group(2).strip().replace(',', '')
+
+        inmate = {}
+
+        mugshot = contentContainer.find('img', {'alt': 'Offender Picture'})
+
+        if mugshot and len(mugshot) > 0:
+            inmate['mugshot'] = stateURL + mugshot.get('src').strip('/')
+
+        inmateInfoHeaders = contentContainer.find_all('td', {'width': '40%'})
+        inmateInfoInfo = contentContainer.find_all('td', {'width': '60%'})
+        i = 0
+        info = 0
+        while i < len(inmateInfoHeaders):
+            try:
+                inmate[inmateInfoHeaders[i].get_text().strip().replace(':', '').replace(' ', '_').lower()] = inmateInfoInfo[i].get_text().strip()
+                info = info + 1
+            except:
+                pass
+            i = i + 1
+        # table = contentContainer.find_all('table')
+        # '''
+        # index - desc
+        # 0 - (This information was current as of ...)
+        # 1 - (This information was current as of ...)
+        # 2 - Inmate Data
+        # 3 - Mugshot
+        # 4 - More Inmate Data
+        # '''
+        # print table
+        # break
+        if info > 0:
+            return inmate
+        return None
+
+    def GenDetailLink(self, bookmark, sessionID):
+        return 'detail.asp?Bookmark=' + str(bookmark) + '&From=list&SessionID=' + str(sessionID)
+
 
 # Main App Function
 class Main():
     def __init__(self):
+        print '[.] Loading Offenderscraper Library.'
         DataScraper = OffenderScraper()
 
+        print 'Loading Offenders'
         # ScrapeTableLinks
         DetailPageLinks = DataScraper.ScrapeTableLinks(DataScraper.GetURL(inmatePopulationSearchURL))
+        content = DataScraper.GetURL(stateURL + DetailPageLinks[0])
+        if len(content) < 0:
+            print 'No content on first offender page.'
+            exit()
 
-        # Get Offender Data
+        # Get Total Records from first Offendor page
+        TotalRecords = DataScraper.GetInmateDetails(content, True)
+
+        # Initiate the array to store our offendors
         inmates = []
-        for link in DetailPageLinks:
-            content = DataScraper.GetURL(stateURL + link)
+
+        # Append first offender
+        inmates.append(DataScraper.GetInmateDetails(content))
+
+        # Get Detail Link Structure
+        matches = re.search('SessionID=(\d+)', DetailPageLinks[0])
+        session_id = matches.group(1)
+
+        print '[!] Entering loop! Processing ' + str(TotalRecords) + ' records.'
+        i = 1
+        while i <= TotalRecords:
+            content = DataScraper.GetURL(stateURL + DataScraper.GenDetailLink(i, session_id))
             if len(content) < 0:
-                print 'stop'
-                break
-            soup = BeautifulSoup(content, 'lxml')
-            contentContainer = soup.find(id="dcCSScontentContainer")
+                print 'No content on bookmark ' + str(i)
+                exit()
+            inmate = DataScraper.GetInmateDetails(content)
+            inmates.append(inmate)
+            i = i + 1
 
-            inmate = {}
-
-            mugshot = contentContainer.find('img', {'alt': 'Offender Picture'})
-
-            if mugshot and len(mugshot) > 0:
-                inmate['mugshot'] = stateURL + mugshot.get('src').strip('/')
-
-            inmateInfoHeaders = contentContainer.find_all('td', {'width': '40%'})
-            inmateInfoInfo = contentContainer.find_all('td', {'width': '60%'})
-            i = 0
-            info = 0
-            while i < len(inmateInfoHeaders):
-                try:
-                    inmate[inmateInfoHeaders[i].get_text().strip().replace(':', '').replace(' ', '_').lower()] = inmateInfoInfo[i].get_text().strip()
-                    info = info + 1
-                except:
-                    pass
-                i = i + 1
-            # table = contentContainer.find_all('table')
-            # '''
-            # index - desc
-            # 0 - (This information was current as of ...)
-            # 1 - (This information was current as of ...)
-            # 2 - Inmate Data
-            # 3 - Mugshot
-            # 4 - More Inmate Data
-            # '''
-            # print table
-            # break
-            if info > 0:
-                inmates.append(inmate)
         print str(len(inmates)) + ' inmates loaded.'
-        for inmate in inmates:
-            print inmate
+        # for inmate in inmates:
+        #     print inmate
         return None
 
 
